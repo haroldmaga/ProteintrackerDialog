80056e251d27072e05793ed163bb39e21bd9d7ff
harold <haroldmaga16@gmail.com>
2019-09-24 23:11:25 +0700

    Tugas 07

--- app/src/main/java/com/example/proteintracker50/ListActivity.java
+++ app/src/main/java/com/example/proteintracker50/ListActivity.java
@@ -2,29 +2,31 @@ package com.example.proteintracker50;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class listactivity extends AppCompatActivity {
-    String[] items ={"Harold", "Erick", "Aru", "Brian", "Ryan"};
+public class ListActivity extends AppCompatActivity {
 
+    String[] items={"lorem", "ipsum", "dolor", "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_mahasiswa);
+        setContentView(R.layout.activity_list);
 
-        ListView listView = findViewById(R.id.lvMhs);
-        Spinner spinner = findViewById(R.id.mhsSpinner);
+        ListView listView = findViewById(R.id.lvLoremIpsum);
+        Spinner spinner = findViewById(R.id.spinnerProgmob);
 
-        listView.setAdapter(new ArrayAdapter<String>(listactivity.this, android.R.layout.simple_list_item_1,items));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(listactivity.this, android.R.layout.simple_spinner_item,items);
+        listView.setAdapter(new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, items));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(ListActivity.this, android.R.layout.simple_spinner_item, items);
 
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
@@ -32,20 +34,23 @@ public class listactivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(listactivity.this, "Anda memilih = " + items [i], Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListActivity.this, "Anda memilih = " + items[i], Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                Toast.makeText(listactivity.this, "Anda tidak memilih", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListActivity.this, "Anda tidak memilih", Toast.LENGTH_SHORT).show();
             }
         });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(listactivity.this, "Anda memilih = " +  items [i], Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListActivity.this, "Anda memilih = " + items[i], Toast.LENGTH_SHORT).show();
             }
         });
     }
+}
+
+
 
